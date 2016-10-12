a6b75b4a6f681a69766adf051e5a9223fd151377
enuri91 <enuri91@gmail.com>
2016-10-12 14:03:23 +0900

    gugu_code #2 : upto nine by nine

--- gugu_code.c
+++ gugu_code.c
@@ -2,9 +2,9 @@
  
 int main(void) {
  
-	for(int i = 1 ; i < 4 ; i++)
+	for(int i = 1 ; i < 10 ; i++)
 	{
-		for(int j = 1 ; j < 4 ; j++)
+		for(int j = 1 ; j < 10 ; j++)
 		{
 			printf(" %d * %d = %d\t", j, i, j*i);
 		}
